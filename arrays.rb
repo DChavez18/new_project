2a443efab3cbc5681b5a0ec512939fdc969b297d
Derek Chavez <frrenose17@yahoo.com>
2023-04-12 14:20:57 -0600

    Call methods on arrays

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,19 @@
 names = ["shelby", "corey", "derek", "nathan"]
-number = [1,2,3,4]
+numbers = [1,2,3,4]
 decimals = [1.1, 2.2, 3.3, 4.4]
 booleans = [(1==1), (2==2), (3==3), (4==4)]
+
+names.pop
+# This will remove "nathan" from the array.
+
+numbers.push(5)
+# This will add the integer 5 to the array.
+
+decimals.shift
+# This will shift all floats in the array up 1 position
+# starting with the "0" position, which will be removed
+# from the array.
+
+booleans.unshift(5==5)
+# This will add the boolean (5==5) to the beginning of the
+# array
